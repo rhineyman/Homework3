f21a845893dd55aebded8f202fb5482f51518d87
Mike Rhiney <rhineyman@gmail.com>
2021-02-12 11:59:30 -0800

    changed some paramaters if/while

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,58 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// function passVariable () {
+
 var numbers = ("1", "2", "3", "4", "5", "6", "7", "8", "9");
 var specChar = ("?", ":", "(", "}", "{", ")", "@", "#", "$", "&");
 var lowercase = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "o", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 var uppercase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "O", "M", "N", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
 //use selects password length
+while (!passLength) { 
 var passLength = prompt("Please select the number of characters in your password between 8-128");
-if (passLength < 8) {
-  alert("Must be longer than 8 characters");
-} if (passLength > 128) {
-  alert("Must be less than 128 characters");
+if (passLength < 8 || passLength > 128) {
+  alert("Must be longer than 8 characters, Must be less than 128 characters");
+  passLength = null;  
 }
+
+} 
+
 console.log(passLength);
 //user selects what type of characters
-var selLowercase = confirm("Would you like lowercase letters?");
-var selUppercase = confirm("Would you like uppercase letters?");
-var selNum = confirm("Would you like numbers?");
-var selSpecial = confirm("Would you like special characters?");
+
+
+while (  
+  !selLowercase  &&
+  !selUppercase  &&
+  !selNum  &&
+  !selSpecial 
+){
+  var selLowercase = confirm("Would you like lowercase letters?");
+  var selUppercase = confirm("Would you like uppercase letters?");
+  var selNum = confirm("Would you like numbers?");
+  var selSpecial = confirm("Would you like special characters?");
+ 
+
 if (
   selLowercase === false &&
   selUppercase === false &&
   selNum === false &&
   selSpecial === false
-) {
-  alert("Must select a character type")
+)
+
+  {
+  alert("Must select a character type");
+
+}
 }
 console.log(selLowercase, selUppercase, selNum, selSpecial);
 
 var userSelect = (selLowercase, selUppercase, selNum, selSpecial, passLength);
 
 console.log(userSelect);
+// }
+
 
 //password generation from above variables
 
